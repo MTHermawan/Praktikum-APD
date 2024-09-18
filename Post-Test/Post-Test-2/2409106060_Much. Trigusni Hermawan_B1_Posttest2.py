eebc2f44ec178b7ea7f48166f619be90eac05b2c
Trigusni <109473523+MTHermawan@users.noreply.github.com>
2024-09-18 09:18:28 +0800

    Laporan Posttest 2

--- Post-Test/Post-Test-2/2409106060_Much. Trigusni Hermawan_B1_Posttest2.py
+++ Post-Test/Post-Test-2/2409106060_Much. Trigusni Hermawan_B1_Posttest2.py
@@ -1,21 +1,20 @@
 nama = input("Masukkan Nama Anda: ")
 umur = int(input("Masukkan Umur Anda: "))
-tinggiBadan = float(input("Masukkan Tinggi Badan Anda (Cm): "))
 beratBadan = float(input("Masukkan Berat Badan Anda (Kg): "))
+tinggiBadan = float(input("Masukkan Tinggi Badan Anda (Cm): "))
 singleStatus = input("Apakah Anda Seorang Single? (y/n) ")[0].lower() == "y"
 
 jumlahNilaiNumerik = umur + tinggiBadan + beratBadan
 
 print(f"""
 ==============================================
-                Bio Data Diri
+                Bio Data Anda
 ==============================================
 Nama                 : {nama}
 Umur                 : {umur} Tahun
 Berat Badan          : {beratBadan} Kg
 Tinggi Badan         : {tinggiBadan} Cm
-Single               : {singleStatus} 
+Single               : {singleStatus}
 Jumlah Nilai Numerik : {jumlahNilaiNumerik}
 ==============================================
-"""
-)
+""")
