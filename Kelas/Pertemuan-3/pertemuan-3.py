d9a9273a3484315d0a325bd2b1063c8ea10e25c2
Trigusni <109473523+MTHermawan@users.noreply.github.com>
2024-09-20 14:50:46 +0800

    Pertemuan 3

---
+++ Kelas/Pertemuan-3/pertemuan-3.py
@@ -0,0 +1,95 @@
+# pengenalan if - menentukan kegiatan berdasarkan cuaca
+# cuaca = "cerah"
+# cuaca = "mendung"
+
+# if cuaca == "cerah":
+#     print("Kamu pergi ke luar rumah")
+
+# =====================================
+
+# pengenalan else - mengecek ketidakpunyaan uang
+# uang = 0
+
+# if uang:
+#     print("Pergi ke Pasar!")
+# else:
+#     print("Pergi ke rumah teman!")
+
+# =====================================
+
+# mengecek nilai tidak lulus
+# nilai = 70
+
+# if nilai < 70:
+#     print("tidak lulus")
+# else:
+#     print("lulus")
+
+# =====================================
+
+# mengecek angka positif dan negatif
+# angka = 5
+
+# if angka < 0:
+#     print("Bilangan negatif")
+# else:
+#     print("Bilangan negatif")
+
+# =====================================
+
+# Pengenalan Elif (else if) - Mengecek status masa seseorang dengan umur
+# umur = 18
+
+# if umur <= 10:
+#     print("Anak-anak")
+# elif umur <= 18:
+#     print("Remaja")
+# elif umur <= 35:
+#     print("Dewasa")
+# elif umur <= 65:
+#     print("Paruh Baya")
+# else:
+#     print("Tua")
+
+# =====================================
+
+# Pengenalan Nested If ('if' yang berada di dalam 'if')
+# nilai = 84
+
+# if nilai >= 80:
+#     if nilai >= 85:
+#         print("A+")
+#     else:
+#         print("A-")
+# else:
+#     print("B")
+
+# =====================================
+
+# Membuat sistem memilih menu
+# print("""
+# Pilihan Menu:
+# 1. Menonton Film
+# 2. Sedang Ngoding
+# 3. Keluar dari aplikasi
+# """)
+
+# pilihan = input("Masukkan pilihan Anda: ")
+
+# if pilihan == "1":
+#     print("Menonton Film")
+# elif pilihan == "2":
+#     print("Sedang Ngoding")
+# elif pilihan == "3":
+#     print("Keluar dari aplikasi")
+# else:
+#     print("Menu tidak tersedia")
+
+# =====================================
+
+
+
+# =====================================
+
+# catatan
+# variable akan bernilai false jika string kosong, 0, [], (), {}, false
