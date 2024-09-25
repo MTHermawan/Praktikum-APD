ab085b584bdfdbcccf2901ace88132a9a378d822
Trigusni <109473523+MTHermawan@users.noreply.github.com>
2024-09-25 09:43:34 +0800

    Probably Code Final Version and 80% Flowchart

--- Post-Test/Post-Test-3/2409106060_Much.TrigusniHermawan_B1_POSTTEST3.py
+++ Post-Test/Post-Test-3/2409106060_Much.TrigusniHermawan_B1_POSTTEST3.py
@@ -2,48 +2,87 @@ import os
 
 os.system('cls') # Fungsi membersihkan Layar Terminal
 
-# Deklarasi Variabel
-pilihan = ""
-
 # Mencetak Pilihan Menu Operasi Yang Tersedia
-print(f"""
+print("""
 ==========================================================
-    Menu Program Menghitung Luas/Volume Bangun Ruang
+    Menu Program Menghitung Luas/Keliling Bangun Ruang
 ==========================================================
-1. Luas Balok
-2. Volume Balok
+1. Luas Permukaan Balok
+2. Keliling Balok
+3. Luas Permukaan Bola
+4. Luas Permukaan Limas Segi Empat (Beraturan)
 N. Keluar Program
 """)
 
 pilihan = input("Masukkan nomor pilihan menu: ")
 
 if pilihan == "1":
-    print("\n======================= Luas Balok =======================\n")
-    panjang = int(input("Masukkan nilai Panjang (cm): "))
-    lebar = int(input("Masukkan nilai Lebar (cm): "))
-    tinggi = int(input("Masukkan nilai Tinggi (cm): "))
+    print("\n================== Luas Permukaan Balok ==================\n")
+    
+    panjang = float(input("Masukkan nilai panjang balok (cm): "))
+    lebar = float(input("Masukkan nilai lebar balok (cm): "))
+    tinggi = float(input("Masukkan nilai tinggi balok (cm): "))
 
-    # Mencetak hasil dengan rumus luas balok: 2pl + 2pt + 2lt
     hasil = 2 * panjang * lebar + 2 * panjang * tinggi + 2 * lebar * tinggi
-    print(f"""
-Penyelesaian:
-Panjang = {panjang} cm
-Lebar = {lebar} cm
-Tinggi = {tinggi} cm
+    
+    print("\n---------------------- Penyelesaian ----------------------\n")
+    print(f"Panjang = {panjang} cm\nLebar = {lebar} cm\nTinggi = {tinggi} cm\n")
+    print(f"Luas Balok = (2 x panjang x lebar) + (2 x panjang x tinggi) + (2 x lebar x tinggi)")
+    print(f"Luas Balok = (2 x {panjang} x {lebar}) + (2 x {panjang} x {tinggi}) + (2 x {lebar} x {tinggi})")
+    print(f"Luas Balok = {2 * panjang * lebar} + {2 * panjang * tinggi} + {2 * lebar * tinggi}")
+    print(f"Luas Balok = {hasil} cm²\n")
 
-Luas Balok = (2 * panjang * lebar) + (2 * panjang * tinggi) + (2 * lebar * tinggi)
-Luas Balok = (2 * {panjang} * {lebar}) + (2 * {panjang} * {tinggi}) + (2 * {lebar} * {tinggi})
-Luas Balok = {2 * panjang * lebar} + {2 * panjang * tinggi} + {2 * lebar * tinggi}
-Luas Balok = {hasil} cm³
-    """)
 elif pilihan == "2":
-    panjang = int(input("Masukkan nilai Panjang: "))
-    lebar = int(input("Masukkan nilai Lebar: "))
-    tinggi = int(input("Masukkan nilai Tinggi: "))
+    print("\n===================== Keliling Balok =====================\n")
+    
+    panjang = float(input("Masukkan nilai panjang balok (cm): "))
+    lebar = float(input("Masukkan nilai lebar balok (cm): "))
+    tinggi = float(input("Masukkan nilai tinggi balok (cm): "))
 
-    # Rumus Volume Balok: p x l x t
-    print(panjang * lebar * tinggi)
+    hasil = 4 * (panjang + lebar + tinggi)
+    
+    print("\n---------------------- Penyelesaian ----------------------\n")
+    print(f"Panjang = {panjang} cm\nLebar = {lebar} cm\nTinggi = {tinggi} cm\n")
+    print(f"Keliling Balok = 4 x (panjang + lebar + tinggi)")
+    print(f"Keliling Balok = 4 x ({panjang} + {lebar} + {tinggi})")
+    print(f"Keliling Balok = 4 x {panjang + lebar + tinggi}")
+    print(f"Keliling Balok = {hasil} cm\n")
+
+elif pilihan == "3":
+    print("\n================== Luas Permukaan Bola ===================\n")
+    
+    radius = float(input("Masukkan nilai jari-jari/radius bola (cm): "))
+
+    hasil = 4 * 3.14 * (radius ** 2)
+    
+    print("\n---------------------- Penyelesaian ----------------------\n")
+    print(f"r = {radius} cm\n")
+    print(f"Luas Bola = 4 x π x r²")
+    print(f"Luas Bola = 4 x 3.14 x {radius}²")
+    print(f"Luas Bola = {4 * 3.14} x {radius ** 2}")
+    print(f"Luas Bola = {hasil} cm²\n")
+
+    exec
+
+elif pilihan == "4":
+    print("\n====== Luas Permukaan Limas Segi Empat (Beraturan) =======\n")
+    
+    panjangAlas = float(input("Masukkan nilai panjang sisi alas/persegi (cm): "))
+    tinggiSisiTegak = float(input("Masukkan nilai tinggi sisi tegak/segitiga (cm): "))
+
+    hasil = (panjangAlas * panjangAlas) + (4 * panjangAlas * tinggiSisiTegak / 2) 
+    
+    print("\n---------------------- Penyelesaian ----------------------\n")
+    print(f"Panjang Alas = {panjangAlas} cm\nTinggi Sisi Tegak = {tinggiSisiTegak} cm\n")
+    print(f"Luas Limas Segi Empat = Luas Alas + (4 x panjang alas * tinggi sisi tegak)")
+    print(f"Luas Limas Segi Empat = ({panjangAlas} x {panjangAlas}) + (4 x {panjangAlas} x {tinggiSisiTegak} / 2)")
+    print(f"Luas Limas Segi Empat = ({panjangAlas * panjangAlas}) + ({4 * panjangAlas * tinggiSisiTegak / 2})")
+    print(f"Luas Limas Segi Empat = {hasil} cm²\n")
+
 elif pilihan.lower() == "n":
-    print("\nProgram berhasil dihentikan!\n")
+    print("\nProgram berhasil dihentikan!")
+
 else:
     print("\nTidak ada pilihan menu yang cocok!")
+
+print("========================== END ===========================")
