2078d16a3d42490fd7b3184f5c271d18141f36d8
Trigusni <109473523+MTHermawan@users.noreply.github.com>
2024-09-27 15:08:22 +0800

    Update review-1.py

--- Kelas/Pertemuan-4/review-1.py
+++ Kelas/Pertemuan-4/review-1.py
@@ -1,14 +1,15 @@
-skalaMatriks = 8
 print("Matriks")
+skalaMatriks = 8
 for baris in range(skalaMatriks):
     for kolom in range(skalaMatriks):
         print("*", end=" ")
     print()
 print()
 
+#=====================================================
 
-skalaSegitigaSikuSiku = 8
 print("Segitiga Siku-Siku")
+skalaSegitigaSikuSiku = 8
 for baris in range(skalaSegitigaSikuSiku):
     for kolom in range(skalaSegitigaSikuSiku):
         if kolom < baris + 1:
@@ -16,8 +17,9 @@ for baris in range(skalaSegitigaSikuSiku):
     print()
 print()
 
-print("Segitiga Sama Kaki")
+#=====================================================
 
+print("Segitiga Sama Kaki")
 skalaSegitigaSamaKaki = 8
 for baris in range(skalaSegitigaSamaKaki, 0, -1):
     for kolom in range(skalaSegitigaSamaKaki):
